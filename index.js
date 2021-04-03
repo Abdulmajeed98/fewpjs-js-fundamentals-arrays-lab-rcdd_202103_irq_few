a7d629100eb4e30ad2587df5016049da67e6f726
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-04-03 11:08:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Write your solution here!
-const append, prepend, removeFirst, removeLast = ["Milo", "Otis", "Garfield"];
+const append = ["Milo", "Otis", "Garfield"]; 
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
 
 function Append(item){
   append.push(item);
